cd16a8cd2b37bae4ca474a0f074fa700c28765f0
Bhavdeep <58917780+bhavdeep-saragadam@users.noreply.github.com>
2021-06-02 03:16:52 -0400

    3rd Commit

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const bodyParser = require('body-parser')
 
 
 const app = express();
+app.set('view engine', 'ejs')
+
+app.get('/', function (req, res) {
+    res.render('loginPage')
+})
 
 app.listen(process.env.PORT || 3000, function() {
     console.log("The Server Is Running")
